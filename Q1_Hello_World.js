7a51d2d7361034479fc5215f13be53ff9d7c2c53
Vishwajeet <95116552+vishu0608@users.noreply.github.com>
2023-10-25 21:36:47 +0530

    Add files via upload

---
+++ Q1_Hello_World.js
@@ -0,0 +1,14 @@
+import { createServer } from 'http';
+
+import { upperCase } from 'upper-case';
+
+createServer(function (req, res) {
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  res.write(upperCase("Hello World!"));
+
+  res.end();
+
+
+}).listen(8080);
